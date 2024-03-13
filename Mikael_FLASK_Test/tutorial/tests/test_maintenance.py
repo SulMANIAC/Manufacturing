2489f7fa5b296d20dbb5fb57eacd9d9d3964942c
OscartkChung <oscartkchung@gmail.com>
2024-03-12 17:25:40 -0700

    added maintenance test

---
+++ Mikael_FLASK_Test/tutorial/tests/test_maintenance.py
@@ -0,0 +1,58 @@
+import os
+import tempfile
+import sys
+sys.path.append('tutorial')
+
+from flask import Flask
+from flask import Blueprint
+import flaskr.maintenance
+bp = Blueprint("maintenance", __name__)
+
+import pytest
+
+@pytest.fixture
+def client():
+    app = Flask(__name__)
+    app.register_blueprint(bp)
+    with app.test_client() as client:
+        yield client
+
+#This test checks if the home page is working correctly. It sends a GET request to the home page and checks 
+#if the response status code is 200, which indicates a successful HTTP request.
+def test_home(client):
+    response = client.get('/') #Add URL
+    assert response.status_code == 200
+
+#It sends a POST request to the logout route and checks if the response status code is 302, which indicates a redirection. 
+#This is expected because after logging out, users are typically redirected to another page.
+def test_logout(client):
+    response = client.post('/logout') #Add URL
+    assert response.status_code == 302
+
+def test_acknowledge(client):
+    response = client.post('/acknowledge') #Add URL
+    assert response.status_code == 204
+
+def test_alarm_report(client):
+    response = client.post('/alarm_report') #Add URL
+    assert response.status_code == 200
+
+def test_operator_actions(client):
+    response = client.post('/operator_actions') #Add URL
+    assert response.status_code == 200
+
+def test_acknowledge_history(client):
+    response = client.post('/acknowledge_history') #Add URL
+    assert response.status_code == 200
+
+def test_alarm_history(client):
+    response = client.post('/alarm_history') #Add URL
+    assert response.status_code == 200
+
+def test_maintenance(client):
+    response = client.get('/maintenance') #Add URL
+    assert response.status_code == 200
+
+def test_index(client):
+    response = client.get('/index') #Add URL
+    assert response.status_code == 200
